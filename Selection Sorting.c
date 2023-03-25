1994c8d48c1044ba7364f6762380eff37cfd2621
white/devil <125282518+Sahil-Ahamed@users.noreply.github.com>
2023-03-25 11:59:32 +0530

    Update Selection Sorting.c

--- Selection Sorting.c
+++ Selection Sorting.c
@@ -1,34 +1,33 @@
 #include<stdio.h>
 char main()
 {
-    int n,temp;
-    printf("Enter the number of elements in the array:");
+    int n;
+    printf("Enter size:");
     scanf("%d",&n);
-    
     int ar[n];
-    printf("Enter the elements:");
+    printf("Enter elements:");
     for(int i=0;i<n;i++){
         scanf("%d",&ar[i]);
     }
     
+    
+    int temp=0;
     for(int i=0;i<n-1;i++){
+        int min=i;
         for(int j=i+1;j<n;j++){
-            if(ar[j]>ar[i]){
-                temp=ar[j];
-                ar[j]=ar[i];
-                ar[i]=temp;
+            if(ar[j]<ar[min]){
+                min=j;
             }
-            }
-        }
-
-    printf("Sorted array:\n");
-    for(int i=0;i<n;i++){
-        printf("%d  ",ar[i]);
+         }
+         temp=ar[min];
+         ar[min]=ar[i];
+         ar[i]=temp;
     }
     
-    
-    
-    
+    printf("After sorting:");
+    for(int i=0;i<n;i++){
+        printf("   %d",ar[i]);
+    }
     
     
     return 'c';
