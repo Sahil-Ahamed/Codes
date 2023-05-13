d555a663ac1c6225709be7278cc6106e5e220db9
white/devil <125282518+Sahil-Ahamed@users.noreply.github.com>
2023-05-13 20:13:46 +0530

    singly Linked List.c

---
+++ singly Linked List.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *linker;
+};
+
+void traverse(struct node* traveller){
+    while((traveller->linker)!=NULL){
+        printf("Values= %d\n",traveller->data);
+        traveller=traveller->linker;
+    }
+}
+
+char main(){
+    struct node *head=(struct node *)malloc(sizeof(struct node));
+    struct node *second=(struct node *)malloc(sizeof(struct node));
+    struct node *third=(struct node *)malloc(sizeof(struct node));
+    struct node *fourth=(struct node *)malloc(sizeof(struct node));
+    struct node *fifth=(struct node *)malloc(sizeof(struct node));
+    
+    //For head
+    head->data=23;
+    head->linker=second;
+    //For second
+    second->data=56;
+    second->linker=third;
+    //For third
+    third->data=61;
+    third->linker=fourth;
+    //For fourth
+    fourth->data=79;
+    fourth->linker=fifth;
+    //For fifth
+    fifth->data=93;
+    fifth->linker=NULL;
+    
+    traverse(head);
+    
+    
+    
+    return 'c';
+}
