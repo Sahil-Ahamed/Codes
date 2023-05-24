4108d59ede127565dcb567a31ee9d823faf91e7c
white/devil <125282518+Sahil-Ahamed@users.noreply.github.com>
2023-05-24 18:30:02 +0530

    singly Linked list operations.c

---
+++ singly Linked list operations.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+          struct node* head=NULL;
+
+
+void insert(int element){
+    struct node* first=(struct node*)malloc(sizeof(struct node));
+    first->data=element;
+    first->next=head;
+    head=first;
+}
+
+void del(){
+    head=head->next;
+}
+
+void display(struct node* traveller){
+    printf("\nLinked list:\n");
+    while(traveller->next!=NULL){
+        printf("%d\t",traveller->data);
+        traveller=traveller->next;
+    }
+}
+
+
+char main()
+{
+    head=(struct node*)malloc(sizeof(struct node));
+    int element,size;
+    printf("Enter number of nodes:");
+    scanf("%d",&size);
+    for(int i=0;i<size;i++){
+        printf("Enter element:");
+        scanf("%d",&element);
+        insert(element);
+    }
+    display(head);
+    del();
+    display(head);
+    
+    
+    return 'c';
+}
