d6c48bb6c9f309adc11f691d1cbafdf67981907c
white/devil <125282518+Sahil-Ahamed@users.noreply.github.com>
2023-05-25 14:31:48 +0530

    Update singly Linked List operations.c

--- singly Linked List operations.c
+++ singly Linked List operations.c
@@ -1,50 +1,128 @@
 #include<stdio.h>
+
 #include<stdlib.h>
 
+
+
 struct node{
-    int data;
-    struct node* next;
+
+	int data;
+
+	struct node* next;
+
 };
 
-          struct node* head;
 
 
-void insert(int element){
-    struct node* first=(struct node*)malloc(sizeof(struct node));
-    first->data=element;
-    first->next=head;
-    head=first;
+struct node* head;
+
+
+
+void insert(int val){
+
+	struct node* first=(struct node*)malloc(sizeof(struct node));
+
+	first->data=val;
+
+	first->next=head;
+
+	head=first;
+
 }
+
+
 
 void del(){
-    head=head->next;
+
+	head=head->next;
+
+	printf("Done");
+
 }
 
+
+
 void display(struct node* traveller){
-    printf("\nLinked list:\n");
-    while(traveller->next!=NULL){
-        printf("%d\t",traveller->data);
-        traveller=traveller->next;
-    }
+
+	printf("Value:\n");
+
+	while(traveller->next!=NULL){
+
+		printf("%d\t",traveller->data);
+
+		traveller=traveller->next;
+
+	}
+
+	printf("\n");
+
 }
+
+
+
 
 
 char main()
+
 {
-    head=(struct node*)malloc(sizeof(struct node));
-    head->next=NULL;
-    int element,size;
-    printf("Enter number of nodes:");
-    scanf("%d",&size);
-    for(int i=0;i<size;i++){
-        printf("Enter element:");
-        scanf("%d",&element);
-        insert(element);
-    }
-    display(head);
-    del();
-    display(head);
-    
-    
-    return 'c';
+
+  head=(struct node*)malloc(sizeof(struct node));
+
+  head->next=NULL;
+
+  
+
+  int val,choice;
+
+  while(1){
+
+	  printf("Choose from below....\n");
+
+	  printf("1.Insert\n2.Delete\n3.Display\n");
+
+	  printf("Enter your choice:");
+
+	  scanf("%d",&choice);
+
+	  switch(choice){
+
+	  	case 1:
+
+	  		printf("Enter value:");
+
+	  		scanf("%d",&val);
+
+	  		insert(val);
+
+	  		break;
+
+	  	case 2:
+
+	  		del();
+
+	  		break;
+
+	  	case 3:
+
+	  		display(head);
+
+	  		break;
+
+	  	default:
+
+	  		printf("Choose proper option :(");
+
+	  }
+
+	  printf("\n\n");
+
 }
+
+
+
+
+
+return 'c';
+
+}
+
