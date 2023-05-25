dce4ceee1d5cbc4534a3ff61e4b807962ce7baba
white/devil <125282518+Sahil-Ahamed@users.noreply.github.com>
2023-05-25 19:26:39 +0530

    Update Quick Sort.c

--- Quick Sort.c
+++ Quick Sort.c
@@ -1,45 +1,52 @@
-void quick(int arr[], int start, int stop){
-   int p=start;
-   int q=stop;
-   int pivot=start, temp;
-   if(start < stop){
-      while(p < q){
-         while(arr[p] <= arr[pivot] && p < stop){
-            p++;
-         }
-         while(arr[q] > arr[pivot]){
-            q--;
-         }
-         if(p < q){
-            temp = arr[p];
-            arr[p] = arr[q];
-            arr[q] = temp;
-         }
-      }
-      temp = arr[pivot];
-      arr[pivot] = arr[q];
-      arr[q] = temp;
-      quick(arr, start, q-1);
-      quick(arr, q+1, stop);
-   }
+#include<stdio.h>
+
+void quicksort(int ar[],int start,int stop){
+    int piviot=start;
+    int p=start;
+    int q=stop,temp;
+    
+    if(start<stop){
+        while(p<q){
+            while(ar[p]<=ar[piviot] && p<stop){
+                p++;
+            }
+            while(ar[q]>ar[piviot]){
+                q--;
+            }
+            if(p<q){
+                temp=ar[p];
+                ar[p]=ar[q];
+                ar[q]=temp;
+            }
+        }
+        temp=ar[piviot];
+        ar[piviot]=ar[q];
+        ar[q]=temp;
+        quicksort(ar,start,q-1);
+        quicksort(ar,q+1,stop);
+    }
 }
 
-char main(){
-   int size;
-   printf("Enter size of array:");
-   scanf("%d",&size);
-   int ar[size];
-   printf("Enter the elements:");
-   for(int i=0;i<size;i++){
-       scanf("%d",&ar[i]);
-   }
-   int start=0, stop =size-1;
-   quick(ar,start,stop);
-   printf("After sorting:");
-   for(int i=0;i<size;i++){
-      printf("  %d",ar[i]);
-   }
-      
 
-return 'c';
+char main()
+{
+    int n;
+    printf("Enter size of array:");
+    scanf("%d",&n);
+    int ar[n];
+    printf("Enter elements:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&ar[i]);
+    }
+    int start=0;
+    int stop=n-1;
+    quicksort(ar,start,stop);
+    
+    printf("After sorting:");
+    for(int i=0;i<n;i++){
+        printf("%d  ",ar[i]);
+    }
+    
+    
+    return 'c';
 }
