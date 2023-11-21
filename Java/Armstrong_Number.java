e4d8e85d1e315f1e6c13b99f1430e01a90d642d6
white/devil <125282518+Sahil-Ahamed@users.noreply.github.com>
2023-11-21 21:35:57 +0530

    Update Armstrong_Number.java

--- Java/Armstrong_Number.java
+++ Java/Armstrong_Number.java
@@ -1,26 +1,27 @@
-import java.util.*;
+class Armstrong{
+        public static void main(String[] java){
+                Scanner obj=new Scanner(System.in);
+                System.out.print("Enter a number:");
+                int num=obj.nextInt();
+                int onum=num;
+                int len=num.length();
+                int sum=0;
 
-class Main{
-    public static void main(String[] java){
-        Scanner obj=new Scanner(System.in);
-        System.out.print("Enter a number:");
-        int num=obj.nextInt();
-        int onum=num;
-        
-        int length = (int) Math.log10(num) + 1;
-        
-        int final_sum=0;
-         while (num!=0) {
-            int last_digit=num%10;
-            int sum=(int) Math.pow(last_digit, length);
-            num=num/10;
-            final_sum=final_sum+sum;
+                while(num!=0){
+                        int rem=num%10;
+                        int cal=Math.pow(rem,len);
+                        sum=sum+cal;
+                        num=num/10;
+                }
+
+                if(onum==sum){
+                        System.out.print("Armstrong");
+                }
+                else{
+                        System.out.print("N Arm");
+                }
         }
-        if(final_sum==onum){
-            System.out.print(onum+" is Armstrong number.");
-        }
-        else{
-            System.out.print(onum+" is Not Armstrong number");
-        }
-    }
 }
+
+
+Find error
